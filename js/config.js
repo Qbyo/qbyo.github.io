ac730432af52f8f239f83232e88fe4271d5a1dd7
Qbyo <75168421+Qbyo@users.noreply.github.com>
2021-12-04 16:08:19 +0100

    aaaa

--- js/config.js
+++ js/config.js
@@ -53,7 +53,9 @@ var l_bgImages = [
 	"backgrounds/images/touhou01.png",
 	"backgrounds/images/touhou02.png",
 	"backgrounds/images/touhou03.png",
-	"backgrounds/images/touhou04.png"
+	"backgrounds/images/touhou04.png",
+	"backgrounds/images/touhou05.png",
+	"backgrounds/images/touhou06.png",
 ];
 
 /*Random background images order?*/
